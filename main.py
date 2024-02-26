76c1fe665e7ac8b621c1ab56502a8b2bb4310b4f
SujalKThapa <136220535+SujalKThapa@users.noreply.github.com>
2024-02-26 18:07:58 +0530

    GET caloric foods implementation

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 #This API will return a list of all the common food items that meet the specified food criteria.
 import requests
 import json
-import pandas
+import pandas as pd
 
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-
+df = pd.read_csv("calories.csv")
+calList = df["Cals_per100grams"].tolist()
+foodList = df["FoodItem"].tolist()
+calList = [string.replace(" cal", "") for string in calList]
+calList = list(map(int, calList))
 
 @app.route('/')
 def index():
@@ -15,8 +19,12 @@ def index():
 
 @app.route('/calories')
 def calories():
-    calorieCount = request.args.get('calories')
-    return jsonify({"message": calorieCount})
+    calorieCount = int(request.args.get('calories'))
+    healthyFoods = {"Food name": "Calories"}
+    for i  in range(len(calList)):
+        if(calList[i] <= calorieCount):
+            healthyFoods.update({foodList[i]:calList[i]})
+    return jsonify(healthyFoods)
 
 if __name__ == "__main__":
     app.run()
