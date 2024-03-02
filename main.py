d97903b9c96b7307a66289709eeb3228d8a6b4d4
SujalKThapa <136220535+SujalKThapa@users.noreply.github.com>
2024-03-02 15:37:36 +0530

    Last Update
    Final update to the Azure API

--- main.py
+++ main.py
@@ -19,20 +19,30 @@ def index():
     return jsonify({"message": "hello world"})
 
 
-@app.route('/calories')
-def calories():
-    calorieCount = int(request.args.get('calories'))
-    healthyFoods = {"Food name": "Calories"}
-    for i  in range(len(calList)):
-        if(calList[i] <= calorieCount):
-            healthyFoods.update({foodList[i]:calList[i]})
-    return jsonify(healthyFoods)
+#@app.route('/calories')
+#def calories():
+#    calorieCount = int(request.args.get('calories'))
+#    healthyFoods = {"Food name": "Calories"}
+#    for i  in range(len(calList)):
+#        if(calList[i] <= calorieCount):
+#            healthyFoods.update({foodList[i]:calList[i]})
+#    return jsonify(healthyFoods)
 
 
 @app.route('/getNote')
 def getNote():
     noteID = request.args.get('noteID')
     return jsonify(NoteDict.get(noteID))
+
+
+@app.route('/putNote')
+def putNote():
+    noteID = request.args.get('noteID')
+    noteTitle = request.args.get('noteTitle')
+    noteText = request.args.get('noteText')
+    output = {noteID: {noteTitle : noteText}}
+    NoteDict.update(output)
+    return "Added Note"
 
 
 
